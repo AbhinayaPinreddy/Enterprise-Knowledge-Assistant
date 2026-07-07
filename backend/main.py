60b3cf62255021fc479e37ae6a08b1d3971917c5
AbhinayaPinreddy <165926118+AbhinayaPinreddy@users.noreply.github.com>
2026-07-07 16:18:45 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://enterprise-knowledge-assistant-kom0hr84c-enterpriseassistant.vercel.app",
+        "https://enterprise-knowledge-assistant-three.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
