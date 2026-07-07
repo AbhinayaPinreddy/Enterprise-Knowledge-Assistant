60170fa59b1d47eceb9b93282021d46f4038596c
AbhinayaPinreddy <165926118+AbhinayaPinreddy@users.noreply.github.com>
2026-07-07 15:57:44 +0530

    Update CORS allowed origins for the app

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-         "https://YOUR-VERCEL-URL.vercel.app",
+        "https://enterprise-knowledge-assistant-kom0hr84c-enterpriseassistant.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
